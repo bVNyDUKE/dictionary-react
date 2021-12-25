a58dc50e3e2b432102e0b1e18ed78da4c1b080c2
Darko Ivkov <bVNyDUKE@protonmail.com>
2021-12-25 22:54:42 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+function App() {
+  return(
+    <React.Fragment>
+
+      <section className='p-5 border bottom-1'>
+        <header className='text-3xl'>
+          Dictionary
+        </header>
+      </section>
+
+      <main className='max-w-3xl'>
+
+      </main>
+
+    </React.Fragment>
+  )
+}
+
+export default App
